61fd08e793a55a8da1e46833abac6ff7403c1523
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2024-03-16 16:16:17 +0530

    Add files via upload
    complete profile

--- CompleteProfile.js
+++ CompleteProfile.js
@@ -1,15 +1,23 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+
 function CompleteProfile() {
+  const [name,setname]=useState()
+  const[image,setimage]=useState()
   const nameref = useRef();
   const imageref = useRef();
+  const token = localStorage.getItem("token");
 
   async function userDetails() {
     const name = nameref.current.value;
     const imageurl = imageref.current.value;
-    const token = localStorage.getItem("token");
+    
     console.log(name, imageurl, token);
     const response = await fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBWAai-YRPJ8fRwGXSB0LiHg1JkaxQv-zo",
@@ -26,6 +34,8 @@ function CompleteProfile() {
         },
       }
     );
+    nameref.current.value = "";
+    imageref.current.value = "";
     if (response.ok) {
       const data = await response.json();
       console.log(data.email);
@@ -34,14 +44,49 @@ function CompleteProfile() {
       console.log(data);
     }
   }
+  useEffect(()=>{
+    async function getUserDetaials(){
+        const userdata= await fetch("https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBWAai-YRPJ8fRwGXSB0LiHg1JkaxQv-zo",
+        {
+            method:"POST",
+            body:JSON.stringify({
+                idToken:token,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+              },
+
+        })
+        if(userdata.ok){
+            const userinfo= await userdata.json()
+            console.log(userinfo.users[0].displayName)
+            setname(userinfo.users[0].displayName)
+            setimage(userinfo.users[0].photoUrl)
+        }
+        else{
+            const userinfo=await userdata.json()
+            console.log(userinfo)
+        }
+ } 
+ getUserDetaials()
+ 
+},[])
+  
   return (
-    <div>
-      <center style={{ paddingTop: "160px" }}>
+    <>
+
+
+      <Container>
+        {" "}
+        <Row>
+          <Col> 
+          <div style={{ paddingTop: "160px", marginLeft: "80px" }}>
         <Card
           style={{
             width: "30rem",
             height: "25rem",
-            display: "center",
+            // display: "center",
+            // paddingTop: "160px",
             boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
           }}
         >
@@ -68,13 +113,56 @@ function CompleteProfile() {
             <Button variant="primary" onClick={userDetails}>
               Update
             </Button>{" "}
-            <Button variant="primary" style={{ paddingRight: "20px" }}>
+            {/* <Button variant="primary" style={{ paddingRight: "20px" }}>
               Cancel
-            </Button>
+            </Button> */}
           </Card.Body>
         </Card>
-      </center>
-    </div>
+      </div>
+      </Col> 
+          <Col> 
+          <div style={{ paddingTop: "160px", marginLeft: "80px" }}>
+        <Card
+          style={{
+            width: "30rem",
+            height: "25rem",
+            // display: "center",
+            // paddingTop: "160px",
+            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+          }}
+        >
+          <Card.Body>
+            <Card.Title style={{paddingLeft:"20px"}}>{name}</Card.Title>
+            <Card.Text>
+            <Container>
+            <Row>
+           <Col xs={6} md={4}>
+          <Image  style={{paddingTop:"20px"}}src= {image} rounded />
+            </Col>
+            </Row>
+            </Container>
+            
+            </Card.Text>
+            {/* <Button variant="primary" >
+              Update
+            </Button>{" "} */}
+            {/* <Button variant="primary" style={{ paddingRight: "20px" }}>
+              Cancel
+            </Button> */}
+          </Card.Body>
+        </Card>
+      </div>
+       </Col> 
+        </Row>
+      </Container>
+
+
+
+
+
+
+     
+    </>
   );
 }
 
--- Navbar.js
+++ Navbar.js
@@ -1,18 +1,95 @@
-import React from "react";
+import React, { useContext } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+
+import { Authcontext } from "../Data/AuthContext";
+import { NavLink } from "react-router-dom";
 function Navbarr() {
+  const authctx = useContext(Authcontext);
+  
+  const loggedin = authctx.isLoggedin;
+  const logout = authctx.Logout;
+
   return (
     <div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Expense Tracker</Navbar.Brand>
+          <Navbar.Brand style={{ color: "red", fontSize: "50px" }}>
+            Expense Tracker
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Product</Nav.Link>
-            <Nav.Link href="#pricing">About Us</Nav.Link>
+            <NavLink
+              to="/"
+              style={{
+                textDecoration: "none",
+                paddingTop: "10px",
+                color: "#ffffff",
+                fontSize: "25px",
+              }}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/expense"
+              style={{
+                textDecoration: "none",
+                paddingTop: "10px",
+                paddingLeft: "10px",
+                color: "#ffffff",
+                fontSize: "25px",
+              }}
+            >
+              Expense
+            </NavLink>
           </Nav>
+          {!loggedin ? (
+            <NavLink to="/login" style={{ textDecoration: "none" }}>
+              <Button
+                variant="link"
+                style={{
+                  textDecoration: "none",
+                  paddingTop: "10px",
+                  color: "#ffffff",
+                  fontSize: "25px",
+                }}
+              >
+                {" "}
+                Login{" "}
+              </Button>{" "}
+            </NavLink>
+          ) : (
+            <>
+              <NavLink
+                to="/welcome"
+                style={{
+                  textDecoration: "none",
+                  fontSize: "25px",
+                  paddingTop: "10px",
+                  color: "#ffffff",
+                  // marginRight: "20px"
+                }}
+              >
+                Profile
+              </NavLink>
+              <NavLink to="/login">
+                <Button
+                  variant="link"
+                  onClick={logout}
+                  style={{
+                    textDecoration: "none",
+                    fontSize: "25px",
+                    paddingTop: "10px",
+                    color: "#ffffff",
+                    // marginRight: "20px"
+                  }}
+                >
+                  Logout{" "}
+                </Button>
+              </NavLink>
+            </>
+          )}
         </Container>
       </Navbar>
     </div>
